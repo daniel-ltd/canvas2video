f377e1f985ed58e9d7e69ca754071aad142d7eb8
Daniel Ly <lytiendung.it@gmail.com>
2023-04-07 23:12:00 +0700

    test reset spring

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,19 @@
-import CanvasCapture from 'canvas-capture';
-import { Canvas2Video } from 'canvas2video';
-import Konva from 'konva';
-import { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
-import ChillCanvasAnimation from './components/ChillCanvasAnimation';
-import IntenseCanvasAnimation from './components/IntenseCanvasAnimation';
-import RecordRTC from "recordrtc";
-import CanvasRecordRTC from './components/CanvasRecordRTC';
-import { FrameProvider, useFrame } from './FrameContext';
-import CanvasCapturer from './components/CanvasCaptuter';
-import Canvas2VideoRecorder from './components/Canvas2VideoRecorder';
-import CCapturer from './components/CCapturer';
 import DemoAnimation from './components/DemoAnimation';
 
 function App() {
-  const stageRef = useRef<Konva.Stage>(null);
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [isRecording, setIsRecording] = useState(false);
-
-  useEffect(() => {
-    const canvasElements = stageRef.current?.container().querySelectorAll("canvas");
-    if (canvasElements) {
-      canvasRef.current = canvasElements[1] || canvasElements[0];
-    }
-  }, [stageRef]);
-
-  const runAnimation = () => {
-    setIsRecording(true);
-  }
-
   return (
     <>
       {/* <ChillCanvasAnimation ref={stageRef} /> */}
       {/* <IntenseCanvasAnimation ref={stageRef} /> */}
-      <DemoAnimation recording={isRecording} />
+      <DemoAnimation />
 
-      <div className='btn-group'>
-        <CanvasRecordRTC onBeforeRecord={runAnimation} />
-        {/* <CanvasCapturer onBeforeRecord={runAnimation} /> */}
-        {/* <Canvas2VideoRecorder onBeforeRecord={runAnimation} /> */}
-        {/* <CCapturer onBeforeRecord={runAnimation} /> */}
-      </div>
+      {/* <div className='btn-group'> */}
+      {/* <CanvasRecordRTC onBeforeRecord={runAnimation} /> */}
+      {/* <CanvasCapturer onBeforeRecord={runAnimation} /> */}
+      {/* <Canvas2VideoRecorder onBeforeRecord={runAnimation} /> */}
+      {/* <CCapturer onBeforeRecord={runAnimation} /> */}
+      {/* </div> */}
     </>
   );
 }
--- src/components/DemoAnimation.tsx
+++ src/components/DemoAnimation.tsx
@@ -2,44 +2,37 @@ import { forwardRef, useEffect, useRef, useState } from "react";
 import { Layer, Stage, Rect, Text } from "react-konva";
 import Konva from "konva";
 import { animated, useSpring, useSpringRef } from "@react-spring/web";
-
-interface CanvasProps {
-  recording: boolean
-}
+import { configure } from "@testing-library/react";
 
 const AnimatedRect = animated(Rect);
 const AnimatedText = animated(Text);
 
-const DemoAnimation = forwardRef<Konva.Stage, CanvasProps>(({ recording }, stageRef) => {
-  const panelRef = useRef<HTMLDivElement>(null);
-  const layerRef = useRef<Konva.Layer>(null);
+const DemoAnimation = forwardRef<Konva.Stage>((props, stageRef) => {
+  const [timestamp, setTimestamp] = useState<number>();
 
   const api = useSpringRef();
   const [size] = useState({ width: 360, height: 640 });
-
-  const [spring, set] = useSpring(() => ({
-    x: 0,
-    y: 0
-  }));
 
   const rect1 = useSpring({
     from: { x: 60, y: 0, opacity: 0 },
     to: { x: 60, y: 35, opacity: 1 },
     config: { duration: 1600 },
     ref: api,
-    // events: () => ({
-    //   onStart: () => console.log('the spring has started'),
-    //   onClick: () => console.log('the spring has resolved')
-    // })
+    events: () => ({
+      onStart: () => console.log('the spring has started'),
+      onClick: () => console.log('the spring has resolved')
+    }),
+    // reset: true,
   });
 
-  const obello = useSpring({
+  const [obello, set] = useSpring(() => ({
     from: { x: 60, y: 0, opacity: 0 },
     to: { x: 60, y: 40, opacity: 1 },
     delay: 600,
     config: { duration: 1000 },
     ref: api,
-  });
+    reset: true,
+  }));
 
   const rect2 = useSpring({
     from: { x: 60, y: 190, opacity: 0 },
@@ -47,6 +40,7 @@ const DemoAnimation = forwardRef<Konva.Stage, CanvasProps>(({ recording }, stage
     delay: 1600,
     config: { duration: 2000 },
     ref: api,
+    // reset: true,
   });
 
   const rect3 = useSpring({
@@ -57,6 +51,7 @@ const DemoAnimation = forwardRef<Konva.Stage, CanvasProps>(({ recording }, stage
     delay: 1000,
     config: { duration: 3600 },
     ref: api,
+    // reset: true,
   });
 
   const rect4 = useSpring({
@@ -65,67 +60,91 @@ const DemoAnimation = forwardRef<Konva.Stage, CanvasProps>(({ recording }, stage
     delay: 4600,
     config: { duration: 1320 },
     ref: api,
+    // reset: true,
   });
 
   useEffect(() => {
-    recording && api.start();
-  }, [recording, api]);
+    if (!!timestamp) {
+      // api.current.forEach(le => le.resume());
+      set({
+        from: { x: 60, y: 0, opacity: 0 },
+        to: { x: 60, y: 40, opacity: 1 },
+        delay: 600,
+        config: { duration: 1000 },
+        reset: true,
+        immediate: true
+      })
+      api.start();
+    }
+  }, [timestamp, api]);
+
+  const resetAnimation = () => {
+    setTimestamp(Date.now());
+  }
 
   return (
-    <div ref={panelRef} className="canvas-stage">
-      <Stage
-        ref={stageRef}
-        width={size.width}
-        height={size.height}>
-        <Layer listening={false}>
-          <Rect width={size.width} height={size.height} fill="white" />
-          <AnimatedRect
-            {...rect1}
-            width={240}
-            height={60}
-            fill="#FFC16E"
-          />
+    <>
+      <div className='btn-group'>
+        <button className="btn-record" onClick={resetAnimation}>
+          Export Video
+        </button>
+      </div>
 
-          <AnimatedText
-            text="Obello"
-            fontSize={60}
-            align="center"
-            fontFamily="Optima"
-            {...obello}
-            width={240}
-            height={60}
-            fill="#FF6E72"
-          />
+      <div className="canvas-stage">
+        <Stage
+          ref={stageRef}
+          width={size.width}
+          height={size.height}>
+          <Layer listening={false}>
+            <Rect width={size.width} height={size.height} fill="white" />
+            <AnimatedRect
+              {...rect1}
+              width={240}
+              height={60}
+              fill="#FFC16E"
+            />
 
-          <AnimatedRect
-            {...rect2}
-            width={240}
-            height={25}
-            fill="#FF6E72"
-          />
+            <AnimatedText
+              text="Obello"
+              fontSize={60}
+              align="center"
+              fontFamily="Optima"
+              {...obello}
+              width={240}
+              height={60}
+              fill="#FF6E72"
+            />
 
-          <AnimatedRect
-            {...rect3}
-            width={240}
-            height={400}
-            fillLinearGradientStartPoint={{ x: -60, y: -0 }}
-            fillLinearGradientEndPoint={{ x: 0, y: 600 }}
-            fillLinearGradientColorStops={[0, 'rgb(246, 211, 101)', 1, 'rgb(253, 160, 133)']}
-          />
+            <AnimatedRect
+              {...rect2}
+              width={240}
+              height={25}
+              fill="#FF6E72"
+            />
 
-          <AnimatedText
-            text="Style for days"
-            fontSize={30}
-            align="center"
-            fontFamily="Optima"
-            {...rect4}
-            width={240}
-            height={30}
-            fill="#E87664"
-          />
-        </Layer>
-      </Stage>
-    </div>
+            <AnimatedRect
+              {...rect3}
+              width={240}
+              height={400}
+              fillLinearGradientStartPoint={{ x: -60, y: -0 }}
+              fillLinearGradientEndPoint={{ x: 0, y: 600 }}
+              fillLinearGradientColorStops={[0, 'rgb(246, 211, 101)', 1, 'rgb(253, 160, 133)']}
+            />
+
+            <AnimatedText
+              text="Style for days"
+              fontSize={30}
+              align="center"
+              fontFamily="Optima"
+              {...rect4}
+              width={240}
+              height={30}
+              fill="#E87664"
+            />
+          </Layer>
+        </Stage>
+      </div>
+    </>
   );
 });
 
